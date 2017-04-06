8dc6c6c4554ef5bbb459951f7d034bdbd5e19a08
cral <dongshuai@ect888.com>
2017-04-06 08:54:39 +0800

    订单详情

---
+++ app/src/main/java/com/optimumnano/quickcharge/activity/order/OrderlistDetailActivity.java
@@ -0,0 +1,22 @@
+package com.optimumnano.quickcharge.activity.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.optimumnano.quickcharge.R;
+import com.optimumnano.quickcharge.base.BaseActivity;
+
+public class OrderlistDetailActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orderlist_detail);
+        initViews();
+    }
+
+    @Override
+    public void initViews() {
+        super.initViews();
+    }
+}
