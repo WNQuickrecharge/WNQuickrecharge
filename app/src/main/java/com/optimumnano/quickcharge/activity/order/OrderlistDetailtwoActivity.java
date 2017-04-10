56b97cd71d8f5d7c844381f25d978e27772a996d
cralds <961189699@qq.com>
2017-04-10 22:30:31 +0800

    订单接口，数据填充

--- app/src/main/java/com/optimumnano/quickcharge/activity/order/OrderlistDetailtwoActivity.java
+++ app/src/main/java/com/optimumnano/quickcharge/activity/order/OrderlistDetailtwoActivity.java
@@ -9,10 +9,10 @@ import com.optimumnano.quickcharge.bean.OrderBean;
 import com.optimumnano.quickcharge.views.MenuItem1;
 
 public class OrderlistDetailtwoActivity extends BaseActivity {
-
-    private TextView tvStatus,tvOrdernum,tvCompany,tvAddress,tvDate;
+    private TextView tvDeleteOrder;
+    private TextView tvStatus,tvOrdernum,tvCompany,tvAddress,tvDate,tvServiceCash;
     private OrderBean orderBean;
-    private MenuItem1 miGunNum,miPileType,miElec,miPower,miForzenCatsh;
+    private MenuItem1 miUsertime,miAllelec,miRechargeCash,miAllMoney,miSMoney,miYFMoney,miBackMoney;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +28,26 @@ public class OrderlistDetailtwoActivity extends BaseActivity {
     public void initViews() {
         super.initViews();
         setTitle("订单详情");
+        tvDeleteOrder = (TextView) findViewById(R.id.orderlistDetl_tvDeleteOrder);
+        tvStatus = (TextView) findViewById(R.id.orderlistDetl_tvStatus);
+        tvOrdernum = (TextView) findViewById(R.id.orderlistDetl_tvOrdernum);
+        tvCompany = (TextView) findViewById(R.id.orderlistDetl_tvCompany);
+        tvAddress = (TextView) findViewById(R.id.orderlistDetl_tvAddress);
+        tvDate = (TextView) findViewById(R.id.orderlistDetl_tvTime);
+        miUsertime = (MenuItem1) findViewById(R.id.orderdetl_miUseTime);
+        miAllelec = (MenuItem1) findViewById(R.id.orderdetl_miAllelec);
+        miRechargeCash = (MenuItem1) findViewById(R.id.orderdetl_miRechargeCash);
+        tvServiceCash = (TextView) findViewById(R.id.orderdetl_tvServiceCash);
+        miAllMoney = (MenuItem1) findViewById(R.id.orderdetl_miAllMoney);
+        miSMoney = (MenuItem1) findViewById(R.id.orderdetl_miSMoney);
+        miYFMoney = (MenuItem1) findViewById(R.id.orderlistDetl_miYFCash);
+        miBackMoney = (MenuItem1) findViewById(R.id.orderlistDetl_miBackCash);
     }
     private void initData(){
-
+        tvOrdernum.setText(orderBean.order_no);
+        tvDate.setText(orderBean.start_time);
+        miYFMoney.setRightText("￥"+orderBean.frozen_cash);
+        miSMoney.setRightText("￥"+orderBean.charge_cash);
+        miAllelec.setRightText(orderBean.charge_vol+"kwh");
     }
 }
