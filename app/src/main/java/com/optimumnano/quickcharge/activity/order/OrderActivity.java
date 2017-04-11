8d2e3cd1634ef38303ae544b308a0948219f630e
cral <dongshuai@ect888.com>
2017-04-11 18:41:23 +0800

    下单接口，握手

--- app/src/main/java/com/optimumnano/quickcharge/activity/order/OrderActivity.java
+++ app/src/main/java/com/optimumnano/quickcharge/activity/order/OrderActivity.java
@@ -1,23 +1,33 @@
 package com.optimumnano.quickcharge.activity.order;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSON;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.optimumnano.quickcharge.R;
 import com.optimumnano.quickcharge.base.BaseActivity;
+import com.optimumnano.quickcharge.bean.RechargeGunBean;
 import com.optimumnano.quickcharge.dialog.PayDialog;
 import com.optimumnano.quickcharge.dialog.PayWayDialog;
 import com.optimumnano.quickcharge.manager.OrderManager;
 import com.optimumnano.quickcharge.net.ManagerCallback;
 import com.optimumnano.quickcharge.views.MenuItem1;
 
+import java.util.HashMap;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
+/**
+ * 下单界面
+ */
 public class OrderActivity extends BaseActivity implements View.OnClickListener {
 
     @Bind(R.id.order_tvConfirm)
@@ -42,6 +52,9 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener 
     private PayDialog payDialog;
     private PayWayDialog payWayDialog;
     private OrderManager orderManager = new OrderManager();
+    private RechargeGunBean gunBean;
+    private String orderNo = "";//订单号
+    private String gunNo = "67867678901234517";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,17 +75,25 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener 
         miPayway.setOnClickListener(this);
     }
     private void initData(){
-        orderManager.getGunInfo("67867678901234517", new ManagerCallback() {
+        orderManager.getGunInfo(gunNo, new ManagerCallback<RechargeGunBean>() {
             @Override
-            public void onSuccess(Object returnContent) {
+            public void onSuccess(RechargeGunBean returnContent) {
                 super.onSuccess(returnContent);
+                gunBean = returnContent;
+                loadData();
             }
-
             @Override
             public void onFailure(String msg) {
                 super.onFailure(msg);
             }
         });
+    }
+    private void loadData(){
+        miRechargenum.setRightText("67867678901234517");
+        miType.setRightText(gunBean.pile_type);
+        miElectric.setRightText(gunBean.elec_current+"A");
+        miPower.setRightText(gunBean.power+"kwh");
+        miSimprice.setRightText(gunBean.price+"元/kwh");
     }
     private void initDialog(){
         payDialog = new PayDialog(this);
@@ -113,7 +134,10 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener 
                 if (s.length() == 6){
                     if (s.toString().equals("123456")){
                         payDialog.setStatus(PayDialog.PAYSUCCESS);
-                        skipActivity(RechargeControlActivity.class,null);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("order_no",orderNo);
+                        bundle.putString("gun_no",gunNo);
+                        skipActivity(RechargeControlActivity.class,bundle);
                         payDialog.close();
                     }
                     else {
@@ -135,8 +159,7 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener 
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.order_tvConfirm:
-                payDialog.setStatus(0);
-                payDialog.show();
+                addOrder();
                 break;
             case R.id.order_payway:
                 payWayDialog.show();
@@ -144,9 +167,25 @@ public class OrderActivity extends BaseActivity implements View.OnClickListener 
 
         }
     }
+    //下单
+    private void addOrder(){
+        orderManager.addOrder("67867678901234517", edtMoney.getText().toString(), new ManagerCallback<String>() {
+            @Override
+            public void onSuccess(String returnContent) {
+                super.onSuccess(returnContent);
+                Gson gson = new Gson();
+                HashMap<String,Object> ha = gson.fromJson(returnContent,new TypeToken<HashMap<String,Object>>(){}.getType());
+                orderNo = ha.get("order_no").toString();
+                payDialog.setStatus(0);
+                payDialog.show();
+            }
 
-    private void choosePayway(int payway) {
-        payDialog.setPayway(payway);
-        payWayDialog.close();
+            @Override
+            public void onFailure(String msg) {
+                super.onFailure(msg);
+                showToast(msg+"");
+            }
+        });
     }
+
 }
