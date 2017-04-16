19ae3d2d4ebfec8da10b285024d038b312daf6c1
chenwenguang <cwg-1@163.com>
2017-04-16 17:07:08 +0800

    1.站点详情页充电桩显示完成。

--- app/src/main/java/com/optimumnano/quickcharge/net/HttpApi.java
+++ app/src/main/java/com/optimumnano/quickcharge/net/HttpApi.java
@@ -47,6 +47,7 @@ public class HttpApi {
     public static final String add_collection = "capp/user/add_collection";//增加收藏站点
     public static final String about_url = baseUrl+"aboutus.html";//关于我们
     public static final String delete_collection = "capp/user/delete_collection";//增加收藏站点
+    public static final String get_station_detail = "capp/user/get_station_detail";//增加收藏站点
 
 
 
