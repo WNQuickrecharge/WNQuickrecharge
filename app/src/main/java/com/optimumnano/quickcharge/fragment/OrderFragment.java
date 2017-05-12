50d6a0a3894c17827d701f3d1ad6f7612fb9c251
mfwn <cwg-1@163.com>
2017-05-12 17:34:56 +0800

    1.切换到生产库 2.订单列表页面OnResume自动刷新订单

--- app/src/main/java/com/optimumnano/quickcharge/fragment/OrderFragment.java
+++ app/src/main/java/com/optimumnano/quickcharge/fragment/OrderFragment.java
@@ -99,7 +99,7 @@ public class OrderFragment extends BaseFragment implements View.OnClickListener,
     @Override
     public void onResume() {
         super.onResume();
-        //initData();
+        onRefresh();
     }
 
     private void initData(){
