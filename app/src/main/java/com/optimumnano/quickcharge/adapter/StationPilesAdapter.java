eadc2c191a349db27a4864f186e8e45f4eda3d79
mfwn <cwg-1@163.com>
2017-05-17 19:46:52 +0800

    1.修复站点充电桩状态显示不正确的bug 2.修复充电站点详情页点击可以充电的桩的按钮时 程序崩溃的BUG.

--- app/src/main/java/com/optimumnano/quickcharge/adapter/StationPilesAdapter.java
+++ app/src/main/java/com/optimumnano/quickcharge/adapter/StationPilesAdapter.java
@@ -16,6 +16,7 @@ import com.optimumnano.quickcharge.R;
 import com.optimumnano.quickcharge.activity.order.OrderActivity;
 import com.optimumnano.quickcharge.bean.GunBean;
 import com.optimumnano.quickcharge.bean.PileBean;
+import com.optimumnano.quickcharge.bean.RechargeGunBean;
 import com.optimumnano.quickcharge.manager.OrderManager;
 import com.optimumnano.quickcharge.net.ManagerCallback;
 import com.optimumnano.quickcharge.utils.ToastUtil;
@@ -65,11 +66,14 @@ public class StationPilesAdapter extends BaseQuickAdapter<PileBean, BaseViewHold
             servicePrice.setText(Html.fromHtml(context.getString(R.string.station_service_price, "¥ " + item.getGunList().get(0).max_service + "/kw.h")));
             pileNo.setText(item.getGunList().get(0).getPileNo());
             for (int i = 0; i < item.getGunList().size(); i++) {
-                if (!(item.getGunList().get(i).getGunStatus() == CHARGEING)) {//所有的枪处于正在充电,桩才是正在充电状态
+                int gunStatus = item.getGunList().get(i).getGunStatus();
+                if (gunStatus != CHARGEING) {//所有的枪处于正在充电,桩才是正在充电状态
                     isChargeing = false;
-                } else if (!(item.getGunList().get(i).getGunStatus() == MAINTAIN)) {
+                }
+                if (gunStatus != MAINTAIN) {
                     isMaintain = false;
-                } else if (!(item.getGunList().get(i).getGunStatus()==NETWORK_OUTTIME)) {
+                }
+                if (gunStatus!=NETWORK_OUTTIME) {
                     isNetWorkOutTime=false;
                 }
             }
@@ -98,19 +102,20 @@ public class StationPilesAdapter extends BaseQuickAdapter<PileBean, BaseViewHold
                 pileStatus.setClickable(true);
                 final String finalFristFreeGunNo = fristFreeGunNo;
                 final GunBean finalFirstFreeGun = firstFreeGun;
+                final String pileNo1 = finalFirstFreeGun.getPileNo();
                 pileStatus.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        OrderManager.getGunInfo(item.getPileNo(), new ManagerCallback() {
+                        OrderManager.getGunInfo(pileNo1+finalFristFreeGunNo, new ManagerCallback<RechargeGunBean>() {
                             @Override
-                            public void onSuccess(Object returnContent) {
+                            public void onSuccess(RechargeGunBean returnContent) {
                                 super.onSuccess(returnContent);
-                                Intent intent = new Intent(context, OrderActivity.class);
+                                Intent resultIntent = new Intent(context,OrderActivity.class);
                                 Bundle bundle = new Bundle();
-                                bundle.putString("gun_no", finalFristFreeGunNo);
-                                bundle.putSerializable("gunBean", finalFirstFreeGun);
-                                intent.putExtras(bundle);
-                                context.startActivity(intent);
+                                bundle.putSerializable("gunBean", returnContent);
+                                bundle.putString("gunNo",pileNo1+finalFristFreeGunNo);
+                                resultIntent.putExtras(bundle);
+                                context.startActivity(resultIntent);
                             }
 
                             @Override
