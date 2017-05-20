f9911388f5d08b1b36f53900624238b0ca7d4b78
jack <1032938431@qq.com>
2017-05-20 17:32:08 +0800

    1.解决微信支付因为框架改变失败的bug 2.删除无用工具类

--- app/src/main/java/com/optimumnano/quickcharge/utils/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.optimumnano.quickcharge.utils;
-
-/**
- * Created by ds on 2017/1/13.
- */
-public class Constant {
-    public final static String default_sharedprefence = "wnquickrecharge";
-}
