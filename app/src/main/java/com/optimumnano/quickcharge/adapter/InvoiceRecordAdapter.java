096a89c10b1e471dce7a21eaed6f520577c7564d
zhangjiancheng <myxh_zjc@163.com>
2017-05-27 10:53:48 +0800

    修改开票记录页面横竖屏问题、配送信息问题

--- app/src/main/java/com/optimumnano/quickcharge/adapter/InvoiceRecordAdapter.java
+++ app/src/main/java/com/optimumnano/quickcharge/adapter/InvoiceRecordAdapter.java
@@ -21,17 +21,46 @@ public class InvoiceRecordAdapter extends BaseQuickAdapter<InvoiceRecordBean,Bas
 
     @Override
     protected void convert(BaseViewHolder helper, InvoiceRecordBean item) {
+        /**
+         *发票类型
+         */
         MenuItem1 miType = helper.getView(R.id.adapter_invoice_record_miInvoiceType);
+        /**
+         *开票金额
+         */
         MenuItem1 miInvoiceMoney = helper.getView(R.id.adapter_invoice_record_miInvoiceMoney);
+        /**
+         *运费
+         */
         MenuItem1 miPostcash = helper.getView(R.id.adapter_invoice_record_miPostcash);
+        /**
+         * 开票时间
+         */
         helper.setText(R.id.adapter_invoice_record_miInvoiceTime,"开票时间:"+item.CreateTime);
+        /**
+         *订单编号
+         */
         helper.setText(R.id.adapter_invoice_record_tvOrderno,item.InvoiceOrderNo);
+        /**
+         *状态
+         */
         helper.setText(R.id.adapter_invoice_record_tvStatus,item.Status);
+
         miType.setRightText(item.Type);
         miInvoiceMoney.setRightText("￥"+item.InvoiceAmount+"");
         miPostcash.setRightText("￥"+item.Postage+"");
         if (StringUtils.isEmpty(item.OperateTime)){
             helper.setVisible(R.id.adapter_invoice_record_rlOperate,false);
+        }else {
+            helper.setVisible(R.id.adapter_invoice_record_rlOperate,true);
+            /**
+             * 快递类型
+             */
+            helper.setText(R.id.adapter_invoice_record_tvSendType,item.LogisticsName);
+            /**
+             * 配送时间
+             */
+            helper.setText(R.id.adapter_invoice_record_tvSendTime,"配送时间:"+item.OperateTime);
         }
     }
 }
